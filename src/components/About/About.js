f8df93fcc4b55aff9bd85b98430d53718317944b
Josiah Maina <josiah5115@gmail.com>
2018-07-10 01:11:25 -0600

    seut up sub-router to route Abt, Hst, Cntct Comps

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+
+import History from '../History/History';
+import Contact from '../Contact/Contact';
 
 export default class About extends Component {
   render() {
-    return (
-      <div>
-        <div className='subnav'>
-          <Link to="/about" className="subnav_links"><h3>About</h3></Link>
-          <Link to="/about/hsitory" className="subnav_links"><h3>History</h3></Link>
-          <Link to="/about/contact" className="subnav_links"><h3>Contact</h3></Link>
+      return (
+        <div>
+          <div className='subnav'>
+            <Link to='/about' className='subnav_links'><h3>About</h3></Link>
+            <Link to='/about/history' className='subnav_links'><h3>History</h3></Link>
+            <Link to='/about/contact' className='subnav_links'><h3>Contact</h3></Link>
+          </div>
+
+          <div className='box'>
+            <Switch>
+              <Route path='/about/history' component={ History }/>
+              <Route path='/about/contact' component={ Contact }/>
+              <Route path='/about' render={() => (
+                <div>
+                    <h1>About the University</h1>
+                    <p>
+                      There is nothing like finishing years of collage, speading all that money and only to get a digree that you are never going to use. It's good to know what you want in life and not just be a follower of the system.
+                    </p>
+                </div>
+              )}/>
+            </Switch>
+          </div>
         </div>
-        <div className='box'>
-        </div>
-      </div>
-    )
+      )
   }
-}
+};
